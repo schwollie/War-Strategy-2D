048c6265ffc24840fd168a525741bde5ac7ead9f
Lars <lars@familiechristiansen.de>
2018-04-29 15:14:41 +0200

    Just Adding stuff

---
+++ Blocks.py
@@ -0,0 +1,30 @@
+import pygame
+
+
+class Grass(pygame.sprite.Sprite):
+    def __init__(self, pos_x, pos_y):
+        self.image = "Somefile"
+        self.x_pos = pos_x
+        self.y_pos = pos_y
+
+
+class Dirt(pygame.sprite.Sprite):
+    def __init__(self, pos_x, pos_y):
+        self.image = "Somefile"
+        self.x_pos = pos_x
+        self.y_pos = pos_y
+
+
+class Water(pygame.sprite.Sprite):
+    def __init__(self, pos_x, pos_y):
+        self.image = "Somefile"
+        self.x_pos = pos_x
+        self.y_pos = pos_y
+
+
+class Grass(pygame.sprite.Sprite):
+    def __init__(self, pos_x, pos_y):
+        self.image = "Somefile"
+        self.x_pos = pos_x
+        self.y_pos = pos_y
+
--- Troups.py
+++ Troups.py
@@ -1,8 +1,22 @@
+import pygame
 
 
-class Swordsman(object):
+class Swordsman(pygame.sprite.Sprite):
     def __init__(self):
         self.velocity = 50
         self.health = 60
         self.damage = 60
-        self.moral = 100
+        self.moral = 75
+        self.range = 1
+
+
+class Bowman(pygame.sprite.Sprite):
+    def __init__(self):
+        self.velocity = 65
+        self.health = 20
+        self.damage = 20
+        self.bow_damage = 40
+        self.moral = 60
+
+
+
