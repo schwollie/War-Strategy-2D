fd63e1257082a045fdaf601116344dc030c43830
Lars <lars@familiechristiansen.de>
2018-05-01 12:36:55 +0200

    Generator

--- MapGenerator.py
+++ MapGenerator.py
@@ -1,44 +1,47 @@
 import pygame
 from random import *
-width, height = 200, 200;
+width = 200
+height = 200
 
 grass = 0
 dirt = 1
 rock = 2
 water = 3
 
-Map_Tiles_List = [[grass for x in range(width)] for y in range(height)]
+Map_Tiles_List = [[grass for col in range(width)] for row in range(height)]
 
 
-def add_water(x, y):
-    print(Map_Tiles_List)
-    Map_Tiles_List.insert([x][y], 3)
+def add_water(row, col):
+    Map_Tiles_List[row][col] = water
 
 
-def add_dirt(x, y):
-    Map_Tiles_List.insert([x][y], 1)
+def add_dirt(row, col):
+    Map_Tiles_List[row][col] = dirt
 
 
-def add_rocks(x, y):
-    Map_Tiles_List.insert([x][y], 2)
+def add_rocks(row, col):
+    Map_Tiles_List[row][col] = rock
 
 
-def add_grass(x, y):
-    Map_Tiles_List.insert([x][y], 0)
+def add_grass(row, col):
+    Map_Tiles_List[row][col] = grass
 
 
 def create_map():
-    for x in range(width):
-        for y in range(height):
+    for row in range(height):
+        for col in range(width):
             rand = randint(0, 3)
+            print(row, col, "hi")
+            print(Map_Tiles_List[row][col])
+
             if rand == 0:
-                add_water(x, y)
+                add_water(row, col)
             elif rand == 1:
-                add_dirt(x, y)
+                add_dirt(row, col)
             elif rand == 2:
-                add_rocks(x, y)
+                add_rocks(row, col)
             elif rand == 3:
-                add_grass(x, y)
+                add_grass(row, col)
 
 
 create_map()
