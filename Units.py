9aa28fef4a4ecdfc9eb56f684999ba5e26b0c2b8
Anian <aniankerscher7@gmail.com>
2018-05-01 18:37:15 +0200

    jetzt?

--- Units.py
+++ Units.py
@@ -1,7 +1,6 @@
 import pygame
 import Troups
 
-
 class UnitSwordsmen(pygame.sprite.Sprite):
     def __init__(self, rotation, pos):
         pygame.sprite.Sprite.__init__(self)
@@ -9,12 +8,19 @@ class UnitSwordsmen(pygame.sprite.Sprite):
         self.pos = pos
         self.alive_troups = []
         self.dead_troups = []
-        self.troups_number = 20
+        self.troups_number = 30
         self.rows = 3  # how many Rows there are
 
-        for i in range(0, self.troups_number):
+        for i in range(0, self.troups_number/row):
             warrior = Troups.Swordsman(((self.pos[0] + i), (self.pos[1])))
             self.alive_troups.append(warrior)
+        for i in range(0, self.troups_number/row):
+            warrior = Troups.Swordsman(((self.pos[0] + i), (self.pos[1])+1))
+            self.alive_troups.append(warrior)
+        for i in range(0, self.troups_number/row):
+            warrior = Troups.Swordsman(((self.pos[0] + i), (self.pos[1])+2))
+            self.alive_troups.append(warrior)
+
 
     def rotate(self, rotation):
         self.rotation = rotation
