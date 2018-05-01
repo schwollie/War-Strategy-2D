16022910eef93e0983dafa7ed99ad52d53d9e399
Lars <lars@familiechristiansen.de>
2018-05-01 10:39:19 +0200

    Generator

--- MapGenerator.py
+++ MapGenerator.py
@@ -1,2 +1,32 @@
 import pygame
 
+width, height = 200, 200;
+
+grass = 0
+dirt = 1
+rock = 2
+water = 3
+
+Map_Tiles_List = [[grass for x in range(width)] for y in range(height)]
+print(Map_Tiles_List)
+
+
+def add_lake():
+    pass
+
+
+def add_dirt():
+    pass
+
+
+def add_Rocks():
+    pass
+
+
+
+
+
+
+
+
+
