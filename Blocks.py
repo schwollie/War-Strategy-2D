fe6ea0190350e5314be8defaed4afab3b12f2563
Lars <lars@familiechristiansen.de>
2018-05-01 10:26:16 +0200

    Adding Pictures for Generator

--- Blocks.py
+++ Blocks.py
@@ -3,28 +3,30 @@ import pygame
 
 class Grass(pygame.sprite.Sprite):
     def __init__(self, pos_x, pos_y):
-        self.image = "Somefile"
+        self.image = "images/Grass.png"
         self.x_pos = pos_x
         self.y_pos = pos_y
 
 
 class Dirt(pygame.sprite.Sprite):
     def __init__(self, pos_x, pos_y):
-        self.image = "Somefile"
+        self.image = "images/Dirt.png"
         self.x_pos = pos_x
         self.y_pos = pos_y
 
 
 class Water(pygame.sprite.Sprite):
     def __init__(self, pos_x, pos_y):
-        self.image = "Somefile"
+        self.image = "images/Water.png"
         self.x_pos = pos_x
         self.y_pos = pos_y
 
 
 class Rock(pygame.sprite.Sprite):
     def __init__(self, pos_x, pos_y):
-        self.image = "Somefile"
+        self.image = "images/Rock.png"
         self.x_pos = pos_x
         self.y_pos = pos_y
+
+
 
