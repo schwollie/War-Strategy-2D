f86342fe40875cd1dba6ce7ddc153405b9b6867d
Anian <aniankerscher7@gmail.com>
2018-05-01 11:53:08 +0200

    zufall

--- MapGenerator.py
+++ MapGenerator.py
@@ -1,5 +1,6 @@
 import pygame
-
+from random import *
+import Blocks
 width, height = 200, 200;
 
 grass = 0
@@ -10,18 +11,29 @@ water = 3
 Map_Tiles_List = [[grass for x in range(width)] for y in range(height)]
 print(Map_Tiles_List)
 
+while True:
+    global s
+    s=-1
+    s = s+1
 
 def add_lake():
-    pass
-
+    Map_Tiles_List.insert(s, Blocks.Water)
 
 def add_dirt():
-    pass
+    Map_Tiles_List.insert(s, Blocks.Dirt)
+
+def add_rocks():
+    Map_Tiles_List.insert(s, Blocks.Rock)
+
+def add_grass():
+    Map_Tiles_List.insert(s, Blocks.Grass)
 
 
-def add_Rocks():
-    pass
-
+def create_map():
+    for i in Range (width[0]):
+        x=random.randstr(add_dirt(), add_grass(), add_lake(), add_rocks())
+    for a in Range (hight[1]):
+        y = random.randstr(add_dirt(), add_grass(), add_lake(), add_rocks())
 
 
 
