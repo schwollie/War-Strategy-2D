4f4b70a25ac7d7e857a61708190b62c099515260
Anian <aniankerscher7@gmail.com>
2018-05-04 17:56:39 +0200

    jetzt?

--- Sidebar.py
+++ Sidebar.py
@@ -24,15 +24,15 @@ class Sidebar(pygame.sprite.Sprite):
     def draw_rect(self, screen):
         w, h = pygame.display.get_surface().get_size()
         pygame.draw.rect(screen, colors.red, (w-(0.2*w), 0, w-(0.2*w), h))
-        pygame.draw.rect(screen, colors.dark_grey, (w - (0.2 * w), 0, w - (0.2 * w), h), int(w/400))
+        pygame.draw.rect(screen, colors.dark_grey, (w - (0.2 * w), -100, w - (0.2 * w), (h+200)), int(w/50))
 
     def create_button(self):
         # big rect
         w, h = pygame.display.get_surface().get_size()
         left = w-(w*0.1)
         top = 0
-        width = (w*0.1)
-        height = h*0.1
+        width = w*0.05
+        height = h*0.05
         self.btn = Button.button(left, top, width, height, "+/-", self.settings, 1, function_to_call=self.change_sidebar_visibility)
 
     def draw_all(self, screen, events):
