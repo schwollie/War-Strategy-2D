a6f0bf95dc796f7a71b07904e91f7a931fb2f6c4
Anian <aniankerscher7@gmail.com>
2018-05-04 16:24:08 +0200

    jetzt?

--- Sidebar.py
+++ Sidebar.py
@@ -1,6 +1,6 @@
 import pygame
 import colors
-#
+
 
 Swordsman_image_side = pygame.image.load('images/Swordsman.png')
 Bowman_image_side = pygame.image.load('images/Bowman.png')
@@ -12,4 +12,5 @@ class Sidebar(pygame.sprite.Sprite):
 
     def draw_rect(self, screen):
         w, h = pygame.display.get_surface().get_size()
-        pygame.draw.rect(screen, colors.black, (w-(0.5*w), 0, w-(0.5*w), h))
+        pygame.draw.rect(screen, colors.black, (w-(0.2*w), 0, w-(0.2*w), h))
+        pygame.draw.rect(screen, colors.dark_grey, (w - (0.2 * w), 0, w - (0.2 * w), h), int(w/400))
